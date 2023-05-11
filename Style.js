cff1134b235ed149de6a34f34583151ad8d3cd94
sicilianshmuck <129185084+sicilianshmuck@users.noreply.github.com>
2023-05-11 16:51:25 -0400

    Create Style.js

---
+++ Style.js
@@ -0,0 +1,30 @@
+▼
+* {
+  text-align: center; /*I've added my own css*/
+  font-family: 'Zen Loop', cursive;
+  font-size: 50px;
+  font-weight: 5px;
+}
+#clickericon{
+  text-align: center;
+}
+button {
+  border-radius: 10px;
+  background-color: 
+white;
+}
+.box {
+  position: absolute;
+        left: 524px;
+    top: 200px;
+    height: 0px;
+    width: 0px;
+    border-radius: 100%;
+    border: 1px solid;
+    background-color: hsl(0, 100%, 50%);
+    box-shadow: 0 0 10px hsla(0, 100%, 50%, 0.5),
+                 0 0 10px hsla(0, 100%, 50%, 0.5),
+                 0 0 10px hsla(0, 100%, 50%, 0.5),
+                 0 0 5px hsla(120, 100%, 25%, 1),
+                 0 0 5px hsla(120, 100%, 25%, 1),
+                 0 0 5px hsla(120, 100%, 25%, 1);
